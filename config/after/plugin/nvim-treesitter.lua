bf28b077f0d2a3cee8424e9ebe1ffc349a4adadc
Obszczymucha <obszczymucha@gmail.com>
2022-09-22 00:43:31 +1000

    Add Haskell parser for treesitter.

--- config/after/plugin/nvim-treesitter.lua
+++ config/after/plugin/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "javascript", "scala" },
+  ensure_installed = { "c", "lua", "rust", "javascript", "scala", "haskell" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
