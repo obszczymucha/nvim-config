c6315d0ccc9b36790572cfd42f1383c3daa98e7b
Obszczymucha <obszczymucha@gmail.com>
2022-09-24 19:13:58 +1000

    Fix lualine color.

--- config/after/plugin/lualine.lua
+++ config/after/plugin/lualine.lua
@@ -121,7 +121,7 @@ end
 
 local no_padding = { left = 0, right = 0 }
 
-ins_left_a { is_tmuxed, padding = no_padding, color = { fg = "#cfcfcf", bg = "#405075" } }
+ins_left_a { is_tmuxed, padding = no_padding, color = { fg = "#cfcfcf", bg = "#2a2e40" } }
 ins_left_a "mode"
 ins_left_c "filename"
 ins_right { lsp_status, padding = no_padding }
