0f64a4f6ed78eac326246ccb868762a366b6c836
Obszczymucha <obszczymucha@gmail.com>
2022-09-30 09:34:42 +1000

    Replace pylsp with pyright.

--- config/after/plugin/nvim-lspconfig.lua
+++ config/after/plugin/nvim-lspconfig.lua
@@ -37,21 +37,49 @@ require 'lspconfig'.bashls.setup {
 }
 
 require 'lspconfig'.tsserver.setup {}
+require 'lspconfig'.pyright.setup {}
 
-require 'lspconfig'.pylsp.setup {
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          maxLineLength = 140
-        }
-      },
-      autopep = {
-        enabled = true
-      },
-      yapf = {
-        enabled = false
-      }
-    }
-  }
-}
+local function filter( arr, func )
+  -- Filter in place
+  -- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
+  local new_index = 1
+  local size_orig = #arr
+  for old_index, v in ipairs( arr ) do
+    if func( v, old_index ) then
+      arr[ new_index ] = v
+      new_index = new_index + 1
+    end
+  end
+  for i = new_index, size_orig do arr[ i ] = nil end
+end
+
+local function filter_diagnostics( diagnostic )
+  -- Only filter out Pyright stuff for now
+  if diagnostic.source ~= "Pyright" then
+    return true
+  end
+
+  -- pyspock's expect method
+  if diagnostic.message == '"expect" is not accessed' then
+    return false
+  end
+
+  -- pyspock's where method
+  if diagnostic.message == '"where" is not accessed' then
+    return false
+  end
+
+  -- Allow variables starting with an underscore
+  if string.match( diagnostic.message, '"_.+" is not accessed' ) then
+    return false
+  end
+
+  return true
+end
+
+local function custom_on_publish_diagnostics( a, params, client_id, c, config )
+  filter( params.diagnostics, filter_diagnostics )
+  vim.lsp.diagnostic.on_publish_diagnostics( a, params, client_id, c, config )
+end
+
+vim.lsp.handlers[ "textDocument/publishDiagnostics" ] = vim.lsp.with( custom_on_publish_diagnostics, {} )
