b1ebb32484865a17e0dff9d1ec084f41dea30e1a
Obszczymucha <obszczymucha@gmail.com>
2022-10-01 00:43:52 +1000

    Fix lua fastwrap.

--- config/lua/obszczymucha/keymaps/lua.lua
+++ config/lua/obszczymucha/keymaps/lua.lua
@@ -3,9 +3,12 @@ local M = {}
 
 local function wrap( operation )
   local char = common.get_char_under_cursor()
-  local delete = (char == '"' or char == "'") and "\"_x" or ""
+  local any_quote = char == '"' or char == "'"
+  local delete = char == '"' and "\"_x" or ""
+  local left_space = any_quote and "" or "i <Esc>"
+  local right_space = any_quote and "a" or "a "
 
-  vim.api.nvim_input( string.format( "<Esc>l\"_xi <Esc>%sa %s<Esc>%s", operation, char, delete ) )
+  vim.api.nvim_input( string.format( "<Esc>l\"_x%s%s%s%s<Esc>%s", left_space, operation, right_space, char, delete ) )
 end
 
 function M.fast_continuous_wrap()
