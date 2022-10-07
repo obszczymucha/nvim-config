2d059ad8cc365aeeb96f53a9ed0817a2b21234ee
Obszczymucha <obszczymucha@gmail.com>
2022-10-07 16:24:01 +1100

    Fix telescope previewers removing relative numbers after opening.

--- config/lua/obszczymucha/telescope.lua
+++ config/lua/obszczymucha/telescope.lua
@@ -1,8 +1,17 @@
 local g = vim.g
 local telescope = require( "telescope.builtin" )
+local previewers = require( "telescope.previewers" )
 local M = {}
 
 M.layout_strategy = 'vertical'
+
+require( "telescope" ).setup {
+  defaults = {
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+    qflist_previewer = previewers.vim_buffer_qflist.new
+  }
+}
 
 local function no_ignore_wrapper( f, opts, override )
   if override or g.telescope_no_ignore then
