e7a69fd8a533365731c66f8297a3c4014ad91ec0
Obszczymucha <obszczymucha@gmail.com>
2022-11-24 22:42:24 +1100

    Add and configure nvim-treesitter-textobjects.

--- config/lua/obszczymucha/packer.lua
+++ config/lua/obszczymucha/packer.lua
@@ -18,6 +18,7 @@ return require( "packer" ).startup( function( use )
     requires = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" } }
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
   use { "nvim-treesitter/nvim-treesitter-context" }
+  use { "nvim-treesitter/nvim-treesitter-textobjects", run = ":TSUpdate" }
   use { "nvim-lua/plenary.nvim" }
   use { "ThePrimeagen/harpoon" }
   use { "jenterkin/vim-autosource" }
