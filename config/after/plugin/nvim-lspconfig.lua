274eff1223ce13b0fff27a002cdfae49013438f6
Obszczymucha <obszczymucha@gmail.com>
2023-10-22 16:10:40 +1100

    Add CSS integration.

--- config/after/plugin/nvim-lspconfig.lua
+++ config/after/plugin/nvim-lspconfig.lua
@@ -6,7 +6,7 @@ end
 local mason_lspconfig = prequire( "mason-lspconfig" )
 if mason_lspconfig then
   mason_lspconfig.setup {
-    ensure_installed = { "clangd", "rust_analyzer", "pyright", "ruff_lsp", "tsserver", "lua_ls", "gopls", "bashls", "sqlls" }
+    ensure_installed = { "clangd", "rust_analyzer", "pyright", "ruff_lsp", "tsserver", "lua_ls", "gopls", "bashls", "sqlls", "cssls" }
   }
 end
 
@@ -120,3 +120,10 @@ vim.lsp.handlers[ "textDocument/publishDiagnostics" ] = vim.lsp.with( custom_on_
 lspconfig.rust_analyzer.setup {}
 lspconfig.clangd.setup {}
 lspconfig.sqlls.setup {}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require'lspconfig'.cssls.setup {
+  capabilities = capabilities,
+}
