d760364fa8f8650546e26f4e6e3a9f8fed9b67b1
Obszczymucha <obszczymucha@gmail.com>
2024-04-14 13:00:58 +1000

    Add which-key.

--- config/lua/plugins/init.lua
+++ config/lua/plugins/init.lua
@@ -116,5 +116,18 @@ return {
   { "tpope/vim-fugitive",                          lazy = false },
   { "williamboman/mason-lspconfig.nvim" },
   { "williamboman/mason.nvim" },
-  { "windwp/nvim-autopairs" }
+  { "windwp/nvim-autopairs" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
 }
