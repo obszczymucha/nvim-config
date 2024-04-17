2f6fb56e507ef3407ef45989d0857579dc99859b
Obszczymucha <obszczymucha@gmail.com>
2024-04-17 12:01:56 +1000

    Add vim-matchquote.

--- config/lua/plugins/init.lua
+++ config/lua/plugins/init.lua
@@ -138,5 +138,6 @@ return {
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
     }
-  }
+  },
+  { "airblade/vim-matchquote", lazy = false }
 }
