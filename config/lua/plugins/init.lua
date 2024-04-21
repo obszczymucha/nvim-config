2ab51f19e6526d27dd5fc224746959a0103f6ca4
Obszczymucha <obszczymucha@gmail.com>
2024-04-21 18:22:16 +1000

    Add markdown preview support for Brave on WSL (needs symlink).

--- config/lua/plugins/init.lua
+++ config/lua/plugins/init.lua
@@ -151,14 +151,21 @@ return {
     ft = { "markdown" },
     build = function() vim.fn[ "mkdp#util#install" ]() end,
     config = function()
-      vim.cmd [[
-        function! OpenMarkdownPreview(url)
-          execute "silent !chromium --app=" . a:url
-        endfunction
-      ]]
+      if is_wsl then
+        vim.cmd [[
+          function! OpenMarkdownPreview(url)
+            execute "silent !brave --app=" . a:url
+          endfunction
+        ]]
+      else
+        vim.cmd [[
+          function! OpenMarkdownPreview(url)
+            execute "silent !chromium --app=" . a:url
+          endfunction
+        ]]
+      end
 
       vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
-      vim.notify("Hello")
     end
   }
 }
