cd4b4a358e6fc277aabc71c9ea020186ee2eee11
Obszczymucha <obszczymucha@gmail.com>
2024-04-23 13:22:23 +1000

    Configure remote debug session attaching for Java.

--- config/ftplugin/java.lua
+++ config/ftplugin/java.lua
@@ -118,3 +118,39 @@ vim.keymap.set( "n", "<leader>gt", [[:lua require( "jdtls.tests" ).goto_subjects
   { silent = true, desc = "Go to test/subject" } )
 vim.keymap.set( "n", "<leader>gT", [[:lua require( "jdtls.tests" ).generate()<CR>]],
   { silent = true, desc = "Generate tests" } )
+
+local dap = prequire( "dap" )
+
+local function is_dap_attached()
+  if not dap then return false end
+  local session = dap.session()
+
+  return session and session.initialized and true or false
+end
+
+vim.api.nvim_create_user_command( "DapAttachRemote", function( data )
+  if not dap then
+    vim.notify( "DAP is not configured.", vim.log.levels.WARN )
+    return
+  end
+
+  if is_dap_attached() then
+    vim.notify( "DAP is already attached.", vim.log.levels.WARN )
+    return
+  end
+
+  local port = data and data.args and tonumber( data.args ) or 5005
+
+  dap.listeners.after.event_initialized[ "java" ] = function()
+    vim.notify( string.format( "DAP attached on port %s.", port ), vim.log.levels.INFO )
+    dap.listeners.after.event_initialized[ "java" ] = nil
+  end
+
+  dap.run( {
+    type = "java",
+    request = "attach",
+    name = "Remote Java Debugging",
+    hostName = "127.0.0.1",
+    port = port
+  } )
+end, { nargs = "?" } )
