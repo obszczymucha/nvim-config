630e9d6ab104c8dc69912579332a85fe79589536
Obszczymucha <obszczymucha@gmail.com>
2024-04-27 12:27:29 +1000

    Disable repl opening for Java when running tests.

--- config/lua/obszczymucha/keymaps/java.lua
+++ config/lua/obszczymucha/keymaps/java.lua
@@ -1,12 +1,12 @@
 local M = {}
 
 function M.test_file()
-  require( "dap" ).repl.open()
+  -- require( "dap" ).repl.open()
   require( "jdtls" ).test_class()
 end
 
 function M.test_nearest_method()
-  require( "dap" ).repl.open()
+  -- require( "dap" ).repl.open()
   require( "jdtls" ).test_nearest_method()
 end
 
