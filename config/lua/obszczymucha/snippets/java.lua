9bce54c820faca31b3362b5d958cd53cbc290262
Obszczymucha <obszczymucha@gmail.com>
2024-05-13 22:04:16 +1000

    Refactor java snippets for full blown JUnit 5 capability.

--- config/lua/obszczymucha/snippets/java.lua
+++ config/lua/obszczymucha/snippets/java.lua
@@ -4,100 +4,166 @@ if not ls then return end
 local s = ls.s
 local i = ls.i
 local t = ls.t
+local f = ls.f
 local events = require( "luasnip.util.events" )
+local fmt = require( "luasnip.extras.fmt" ).fmt
 
-local function insert_import( package_name, class_name, modifier )
-  local bufnr = 0
-  local parser = vim.treesitter.get_parser( bufnr, "java" )
+local LANG = "java"
+
+local decapitalize = function( args )
+  return string.gsub( args[ 1 ][ 1 ], "^%u", function( c ) return string.lower( c ) end )
+end
+
+local function camelCaseToSentence( args )
+  local input = args[ 1 ][ 1 ]
+  if input == "" then return "" end
+
+  local result = input:gsub( "(%l)(%u)", "%1 %2" )
+  return result:sub( 1, 1 ):upper() .. result:sub( 2 ):lower() .. "."
+end
+
+local function get_captures( bufnr, lang, query_string )
+  local parser = vim.treesitter.get_parser( bufnr, lang )
   local tree = parser:parse()[ 1 ]
   local root = tree:root()
+  local query = vim.treesitter.query.parse( lang, query_string )
 
-  local mod = modifier and string.format( "%s ", modifier ) or ""
-  local import_name = string.format( "import %s%s.%s;", mod, package_name, class_name )
-  local all_import_name = string.format( "import %s%s.*;", mod, package_name )
+  return query:iter_captures( root, bufnr, 0, -1 )
+end
 
-  local query = vim.treesitter.query.parse( "java", [[
-    (package_declaration) @package
-    (import_declaration) @import
-  ]] )
+local function get_package_details()
+  local bufnr = 0
 
-  local last_import_line_number = nil
-  local package_line_number = nil
+  for _, node in get_captures( bufnr, LANG, "(package_declaration (scoped_identifier) @package)" ) do
+    local package_name = vim.treesitter.get_node_text( node, bufnr )
+    local y = node:range()
 
-  for _, node in query:iter_captures( root, bufnr, 0, -1 ) do
-    local line = vim.treesitter.get_node_text( node, bufnr )
+    return package_name, y + 1
+  end
+end
+
+local function get_class_details()
+  local bufnr = 0
+
+  for _, node in get_captures( bufnr, LANG, "(class_declaration name: (identifier) @class_name)" ) do
+    local class_name = vim.treesitter.get_node_text( node, bufnr )
+    local y = node:range()
+
+    return class_name, y + 1
+  end
+end
+
+local function find_current_imports()
+  local bufnr = 0
+  local captures = get_captures( bufnr, LANG, "(import_declaration) @import" )
+  local result = {}
+
+  for _, node in captures do
+    local import_name = vim.treesitter.get_node_text( node, bufnr )
     local start_row_index = node:range()
-    local line_number = start_row_index + 1
 
-    if line:match( "^import%s" ) then
-      last_import_line_number = line_number
-    elseif line:match( "^package%s" ) then
-      package_line_number = line_number
-    end
-    if line == import_name or line == all_import_name then
-      return
-    end
+    table.insert( result, {
+      line_number = start_row_index + 1,
+      import_name = import_name
+    } )
   end
 
-  local line_number = 0
+  return result
+end
+
+local function insert_imports( imports )
+  if #imports == 0 then return end
+
+  local current_imports = find_current_imports()
+  local last_import_line_number = #current_imports > 0 and current_imports[ #current_imports ].line_number
   local result = {}
+  local line_number = 0
+  local nothing_to_insert = true
 
   if last_import_line_number then
     line_number = last_import_line_number
     table.insert( result, "" )
-  elseif package_line_number then
-    line_number = package_line_number
-    table.insert( result, "" )
+  else
+    local package_name, lineno = get_package_details()
+
+    if package_name and lineno then
+      line_number = lineno
+      table.insert( result, "" )
+    end
   end
 
-  table.insert( result, import_name )
+  for _, import in ipairs( imports ) do
+    local package_name, class_name, modifier = unpack( import )
+    local found = false
+    local mod = import.modifier and string.format( "%s ", modifier ) or ""
+    local import_name = string.format( "import %s%s.%s;", mod, package_name, class_name )
+    local all_import_name = string.format( "import %s%s.*;", mod, package_name )
 
+    for _, current_import in ipairs( current_imports ) do
+      if current_import.import_name == import_name or current_import.import_name == all_import_name then
+        found = true
+        break
+      end
+    end
+
+    if found == false then
+      table.insert( result, import_name )
+      nothing_to_insert = false
+    end
+  end
+
+  if nothing_to_insert == true then return end
   vim.api.nvim_buf_set_lines( 0, line_number, line_number, false, result )
 end
 
 local function insert_test_import()
-  insert_import( "org.junit.jupiter.api", "Test" )
+  insert_imports( { { "org.junit.jupiter.api", "Test" } } )
 end
 
-local function insert_parametrized_test_import()
-  insert_import( "org.junit.jupiter.params", "ParameterizedTest" )
+local function insert_parametrized_test_imports()
+  insert_imports( {
+    { "java.util.stream",                  "Stream" },
+    { "org.junit.jupiter.api",             "DisplayName" },
+    { "org.junit.jupiter.params",          "ParameterizedTest" },
+    { "org.junit.jupiter.params.provider", "Arguments" },
+    { "org.junit.jupiter.params.provider", "MethodSource" }
+  } )
 end
 
-local function hook( f )
+local function hook( callback )
   return {
-    node_callbacks = { [ events.enter ] = f }
+    node_callbacks = { [ events.enter ] = callback }
   }
 end
 
 local function insert_assertj_assertthat_import()
-  insert_import( "org.assertj.core.api.Assertions", "assertThat", "static" )
+  insert_imports( { { "org.assertj.core.api.Assertions", "assertThat", "static" } } )
 end
 
-ls.add_snippets( "java", {
-  s( "test", {
-    t( "@Test" ),
-    t( { "", "public void should" } ), i( 1 ), t( "() {" ),
-    t( { "", "\t// Given" } ),
-    t( { "", "\t" } ), i( 0, "", { node_callbacks = { [ events.enter ] = insert_test_import } } ),
-    t( { "", "", "\t// When" } ),
-    t( { "", "", "\t// Then" } ),
-    t( { "", "}" } )
-  } ),
-  s( "ptest", {
-    t( "@ParameterizedTest" ),
-    t( { "", "public void should" } ), i( 1 ), t( "() {" ),
-    t( { "", "\t// Given" } ),
-    t( { "", "\t" } ), i( 0, "", hook( insert_parametrized_test_import ) ),
-    t( { "", "", "\t// When" } ),
-    t( { "", "", "\t// Then" } ),
-    t( { "", "}" } )
-  } ),
-  s( "gwt", {
-    t( { "// Given", "" } ),
-    i( 0 ),
-    t( { "", "", "// When" } ),
-    t( { "", "", "// Then" } )
-  } ),
+local function get_package_name()
+  local name = get_package_details()
+  return name and string.format( "%s.", name )
+end
+
+ls.add_snippets( LANG, {
+  s( "test", fmt( "@Test\nvoid should{test_name}() {{\n\t// Given\n\t{start}\n\n\t// When\n\n\t// Then\n}}", {
+    start = i( 0, "", hook( insert_test_import ) ),
+    test_name = i( 1 )
+  } ) ),
+  s( "ptest",
+    fmt(
+      "@ParameterizedTest\n@DisplayName(\"{display_name}\")\n@MethodSource(\"{package_name}{class_name}#{provider_name}Provider\")\nvoid should{test_name}() {{\n\t// Given\n\t{start}\n\n\t// When\n\n\t// Then\n}}\n\n@SuppressWarnings(\"unused\")\nprivate static Stream<Arguments> {provider_name}Provider() {{\n\treturn Stream.of();\n}}",
+      {
+        start = i( 0, "", hook( insert_parametrized_test_imports ) ),
+        test_name = i( 1 ),
+        display_name = f( camelCaseToSentence, { 1 } ),
+        package_name = f( get_package_name ),
+        provider_name = f( decapitalize, { 1 } ),
+        class_name = f( get_class_details )
+      } ) ),
+  s( "gwt", fmt( "// Given\n\t{start}\n\n\t// When\n\n\t// Then", {
+    start = i( 0 )
+  } ) ),
   s( "athat", {
     t( "assertThat(" ), i( 1 ), t( ")" ), i( 0, "", hook( insert_assertj_assertthat_import ) )
   } )
