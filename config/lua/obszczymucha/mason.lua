4c4e444ba732b9d974a9f8434365521e71e03e4a
Obszczymucha <obszczymucha@gmail.com>
2024-05-18 12:25:55 +1000

    Update Mason if packages are not available.

--- config/lua/obszczymucha/mason.lua
+++ config/lua/obszczymucha/mason.lua
@@ -9,8 +9,28 @@ local packages = {
   "java-debug-adapter"
 }
 
-for _, package_name in ipairs( packages ) do
-  if registry.is_installed( package_name ) == false then
-    registry.get_package( package_name ):install()
+local function are_packages_available( p )
+  for _, package_name in ipairs( p ) do
+    local success, pkg = pcall( function() return registry.get_package( package_name ) end )
+
+    if not success or not pkg then
+      return false
+    end
   end
+
+  return true
+end
+
+local function install_packages()
+  for _, package_name in ipairs( packages ) do
+    if registry.is_installed( package_name ) == false then
+      registry.get_package( package_name ):install()
+    end
+  end
+end
+
+if are_packages_available( packages ) == true then
+  install_packages()
+else
+  registry.update( install_packages )
 end
