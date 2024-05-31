cf6f05543fd85e22895434087b31af5c607ead07
Obszczymucha <obszczymucha@gmail.com>
2024-05-31 19:10:35 +1000

    Fix the logic and tests fucked up by ChatGPT. Yes, knew it.

--- config/lua/obszczymucha/auto-update.lua
+++ config/lua/obszczymucha/auto-update.lua
@@ -1,6 +1,5 @@
 local M = {}
 
--- Update once after 7am and once after 6pm.
 function M.should_update( last_update_timestamp, current_time )
   local current_date_table = os.date( "*t", current_time )
   local current_hour = current_date_table.hour
@@ -13,13 +12,13 @@ function M.should_update( last_update_timestamp, current_time )
   local needs_morning_update = current_hour >= morning_update_hour and (
     not last_update_hour or
     last_update_date_table.day ~= current_date_table.day or
-    (last_update_hour < morning_update_hour or last_update_hour >= evening_update_hour)
+    last_update_hour < morning_update_hour
   )
 
   local needs_evening_update = current_hour >= evening_update_hour and (
     not last_update_hour or
     last_update_date_table.day ~= current_date_table.day or
-    last_update_hour < evening_update_hour
+    (last_update_hour < evening_update_hour and (current_hour > last_update_hour or last_update_date_table.day ~= current_date_table.day))
   )
 
   return needs_morning_update or needs_evening_update
--- config/test/lua/obszczymucha/auto-update_test.lua
+++ config/test/lua/obszczymucha/auto-update_test.lua
@@ -1,40 +1,39 @@
 package.path = "../../?.lua;" .. package.path .. ";../../../lua/obszczymucha/?.lua"
-
 local lu = require( "luaunit" )
 ---@diagnostic disable-next-line: different-requires
 local should_update = require( "auto-update" ).should_update
 
 CommonSpec = {}
 
-function CommonSpec:test_morning_update_no_previous_update()
+function CommonSpec:should_update_in_the_morning_when_no_previous_update()
   local morning_time = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   lu.assertTrue( should_update( nil, morning_time ) )
 end
 
-function CommonSpec:test_morning_update_after_evening_update_previous_day()
+function CommonSpec:should_update_in_the_morning_after_evening_update_previous_day()
   local last_evening = os.time( { year = 2023, month = 5, day = 29, hour = 19 } )
   local morning_time = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   lu.assertTrue( should_update( last_evening, morning_time ) )
 end
 
-function CommonSpec:test_evening_update_no_previous_update()
+function CommonSpec:should_update_in_the_evening_when_no_previous_update()
   local evening_time = os.time( { year = 2023, month = 5, day = 30, hour = 19 } )
   lu.assertTrue( should_update( nil, evening_time ) )
 end
 
-function CommonSpec:test_evening_update_after_morning_update_same_day()
+function CommonSpec:should_update_in_the_evening_after_morning_update_same_day()
   local morning_time_same_day = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   local evening_time = os.time( { year = 2023, month = 5, day = 30, hour = 19 } )
   lu.assertTrue( should_update( morning_time_same_day, evening_time ) )
 end
 
-function CommonSpec:test_no_update_needed_after_morning_update()
+function CommonSpec:should_not_update_after_morning_update()
   local morning_update_time = os.time( { year = 2023, month = 5, day = 30, hour = 7 } )
   local after_morning_update = os.time( { year = 2023, month = 5, day = 30, hour = 10 } )
   lu.assertFalse( should_update( morning_update_time, after_morning_update ) )
 end
 
-function CommonSpec:test_no_update_needed_after_evening_update()
+function CommonSpec:should_not_update_after_evening_update()
   local evening_update_time = os.time( { year = 2023, month = 5, day = 30, hour = 18 } )
   local after_evening_update = os.time( { year = 2023, month = 5, day = 30, hour = 20 } )
   lu.assertFalse( should_update( evening_update_time, after_evening_update ) )
