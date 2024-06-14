e5fa4c16b5f98c73508760f4e9acaeb82c70b943
Obszczymucha <obszczymucha@gmail.com>
2024-06-14 20:06:58 +1000

    Fix AutoUpdateSpec test suite name.

--- test/lua/obszczymucha/auto-update_test.lua
+++ test/lua/obszczymucha/auto-update_test.lua
@@ -3,43 +3,43 @@ local lu = require( "luaunit" )
 ---@diagnostic disable-next-line: different-requires
 local should_update = require( "auto-update" ).should_update
 
-CommonSpec = {}
+AutoUpdateSpec = {}
 
-function CommonSpec:should_update_in_the_morning_when_no_previous_update()
+function AutoUpdateSpec:should_update_in_the_morning_when_no_previous_update()
   local morning_time = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   lu.assertTrue( should_update( nil, morning_time ) )
 end
 
-function CommonSpec:should_update_in_the_morning_after_evening_update_previous_day()
+function AutoUpdateSpec:should_update_in_the_morning_after_evening_update_previous_day()
   local last_evening = os.time( { year = 2023, month = 5, day = 29, hour = 19 } )
   local morning_time = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   lu.assertTrue( should_update( last_evening, morning_time ) )
 end
 
-function CommonSpec:should_update_in_the_evening_when_no_previous_update()
+function AutoUpdateSpec:should_update_in_the_evening_when_no_previous_update()
   local evening_time = os.time( { year = 2023, month = 5, day = 30, hour = 19 } )
   lu.assertTrue( should_update( nil, evening_time ) )
 end
 
-function CommonSpec:should_update_in_the_evening_after_morning_update_same_day()
+function AutoUpdateSpec:should_update_in_the_evening_after_morning_update_same_day()
   local morning_time_same_day = os.time( { year = 2023, month = 5, day = 30, hour = 8 } )
   local evening_time = os.time( { year = 2023, month = 5, day = 30, hour = 19 } )
   lu.assertTrue( should_update( morning_time_same_day, evening_time ) )
 end
 
-function CommonSpec:should_not_update_after_morning_update()
+function AutoUpdateSpec:should_not_update_after_morning_update()
   local morning_update_time = os.time( { year = 2023, month = 5, day = 30, hour = 7 } )
   local after_morning_update = os.time( { year = 2023, month = 5, day = 30, hour = 10 } )
   lu.assertFalse( should_update( morning_update_time, after_morning_update ) )
 end
 
-function CommonSpec:should_not_update_after_morning_update_at_the_same_hour()
+function AutoUpdateSpec:should_not_update_after_morning_update_at_the_same_hour()
   local morning_update_time = os.time( { year = 2023, month = 5, day = 30, hour = 7 } )
   local after_morning_update = os.time( { year = 2023, month = 5, day = 30, hour = 7} )
   lu.assertFalse( should_update( morning_update_time, after_morning_update ) )
 end
 
-function CommonSpec:should_not_update_after_evening_update()
+function AutoUpdateSpec:should_not_update_after_evening_update()
   local evening_update_time = os.time( { year = 2023, month = 5, day = 30, hour = 18 } )
   local after_evening_update = os.time( { year = 2023, month = 5, day = 30, hour = 20 } )
   lu.assertFalse( should_update( evening_update_time, after_evening_update ) )
