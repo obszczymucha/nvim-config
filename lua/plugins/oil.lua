50e6c4b0432236a2c404df18ad579396ed8cac2b
Obszczymucha <obszczymucha@gmail.com>
2024-06-14 11:41:32 +1000

    Update oil.nvim config.

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,13 +2,17 @@ return {
   "stevearc/oil.nvim",
   config = function()
     require( "oil" ).setup {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
       columns = { "icon" },
       keymaps = {
         [ "<M-h>" ] = "actions.select_split"
       },
       view_options = {
-        show_hidden = true
-      }
+        show_hidden = true,
+        natural_sort = true,
+      },
     }
 
     vim.keymap.set( "n", "-", "<cmd>Oil<CR>", { desc = "Open current directory" } )
