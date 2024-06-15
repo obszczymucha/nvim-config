a2446655a01a911f19f6f4c170c4961b06e7ba3b
Obszczymucha <obszczymucha@gmail.com>
2024-06-15 12:14:51 +1000

    Add bash snippet.

---
+++ lua/obszczymucha/snippets/sh.lua
@@ -0,0 +1,24 @@
+local ls = prequirev( "luasnip" )
+if not ls then return end
+
+local LANG = "sh"
+require( "luasnip.session.snippet_collection" ).clear_snippets( LANG )
+
+local s = ls.s
+local i = ls.i
+local fmt = require( "luasnip.extras.fmt" ).fmt
+
+ls.add_snippets( LANG, {
+  s( "bash", fmt( [[
+  #!/usr/env/bin bash
+
+  function main() {{
+    {start}
+  }}
+
+  main "$@"
+
+  ]], {
+    start = i( 0 )
+  } ) ),
+} )
