5cb9c1f3cdb35534a0ab3993049332637f30b26d
Obszczymucha <obszczymucha@gmail.com>
2024-06-16 09:48:43 +1000

    Add modui lua snippet.

--- lua/obszczymucha/snippets/lua.lua
+++ lua/obszczymucha/snippets/lua.lua
@@ -5,12 +5,31 @@ local LANG = "lua"
 require( "luasnip.session.snippet_collection" ).clear_snippets( LANG )
 
 local s = ls.s
+local f = ls.f
 local i = ls.i
 local fmt = require( "luasnip.extras.fmt" ).fmt
 
-ls.add_snippets( LANG, {
-  s( "gwt", fmt( "-- Given\n{start}\n\n-- When\n\n-- Then", {
-    start = i( 0 )
-  } ) ),
-} )
+local function get_current_filename() return vim.fn.expand( "%:t:r" ) end
 
+ls.add_snippets( LANG, {
+  s( "gwt",
+    fmt( "-- Given\n{start}\n\n-- When\n\n-- Then", {
+      start = i( 0 )
+    } )
+  ),
+  s( "modui",
+    fmt( [[
+      local M = {{}}
+
+      function M.new()
+        {start}
+      end
+
+      ModUi = ModUi or {{}}
+      ModUi.{filename} = M
+    ]], {
+      start = i( 0 ),
+      filename = f( get_current_filename )
+    } )
+  )
+} )
