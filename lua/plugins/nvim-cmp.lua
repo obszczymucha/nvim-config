08c236a2c72c9d8f8a7a769310938576b2cd4484
Obszczymucha <obszczymucha@gmail.com>
2024-07-10 14:59:56 +1000

    Disable nvim_lsp_signature_help due to double help window.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -47,12 +47,12 @@ return {
           end,
         },
         sources = cmp.config.sources( {
-          { name = "luasnip",                 group_index = 2 },
-          { name = "copilot",                 group_index = 2 },
-          { name = "nvim_lsp",                group_index = 2 },
-          { name = "nvim_lsp_signature_help", group_index = 2 },
-          { name = "nvim_lua",                group_index = 2 },
-          { name = "path",                    group_index = 2 },
+          { name = "luasnip",  group_index = 2 },
+          { name = "copilot",  group_index = 2 },
+          { name = "nvim_lsp", group_index = 2 },
+          -- { name = "nvim_lsp_signature_help", group_index = 2 },
+          { name = "nvim_lua", group_index = 2 },
+          { name = "path",     group_index = 2 },
         }, {
           { name = "buffer", group_index = 2 }
         } ),
