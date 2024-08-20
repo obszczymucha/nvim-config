e3c008efe0b3a660898697e906eb68954bb359ab
Obszczymucha <obszczymucha@gmail.com>
2024-08-20 18:58:03 +1000

    Switch back to tokyonight for a change.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,53 +2,50 @@ return {
   -- the colorscheme should be available when starting Neovim
   {
     "folke/tokyonight.nvim",
-    lazy = true,     -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = true,     -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value `:help attr-list`
+        comments = "italic",
+        keywords = "italic",
+        functions = "NONE",
+        variables = "NONE",
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent",       -- style for sidebars, see below
+        floats = "dark",                -- style for floating windows
+      },
+      sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false,             -- dims inactive windows
+      lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
+      -- You can override specific color groups to use other groups or a hex color
+      -- fucntion will be called with a ColorScheme table
+      --@param colors ColorScheme
+      ---@diagnostic disable-next-line: unused-local
+      on_colors = function( colors )
+      end,
+      -- You can override specific highlights to use other groups or a hex color
+      -- function will be called with a Highlights and ColorScheme table
+      --@param highlights Highlights
+      --@param colors ColorScheme
+      ---@diagnostic disable-next-line: unused-local
+      on_highlights = function( highlights, colors )
+      end,
+    },
     config = function()
-      require( "tokyonight" ).setup( {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "storm",        -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-        transparent = true,     -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value `:help attr-list`
-          comments = "italic",
-          keywords = "italic",
-          functions = "NONE",
-          variables = "NONE",
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "transparent",       -- style for sidebars, see below
-          floats = "dark",                -- style for floating windows
-        },
-        sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-        day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-        hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-        dim_inactive = false,             -- dims inactive windows
-        lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
-        -- You can override specific color groups to use other groups or a hex color
-        -- fucntion will be called with a ColorScheme table
-        --@param colors ColorScheme
-        ---@diagnostic disable-next-line: unused-local
-        on_colors = function( colors )
-        end,
-        -- You can override specific highlights to use other groups or a hex color
-        -- function will be called with a Highlights and ColorScheme table
-        --@param highlights Highlights
-        --@param colors ColorScheme
-        ---@diagnostic disable-next-line: unused-local
-        on_highlights = function( highlights, colors )
-        end,
-      } )
-
-      -- load the colorscheme here
-      vim.cmd( [[colorscheme tokyonight]] )
+      vim.cmd( [[colorscheme tokyonight-moon]] )
     end,
   },
   {
     "rebelot/kanagawa.nvim",
-    lazy = false,
+    lazy = true,
     config = function()
       require( 'kanagawa' ).setup( {
         compile = false,  -- enable compiling the colorscheme
@@ -100,7 +97,7 @@ return {
   { "neovim/nvim-lspconfig" },
   { "nvim-lua/plenary.nvim" },
   { "nvim-telescope/telescope-file-browser.nvim" },
-  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make", lazy = false },
+  { "nvim-telescope/telescope-fzf-native.nvim",    build = "make",                                                     lazy = false },
   { "nvim-telescope/telescope-dap.nvim" },
   { "nvim-telescope/telescope.nvim",               branch = "0.1.x" },
   -- { "nvim-treesitter/nvim-treesitter-context" },
