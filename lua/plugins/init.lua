9cf62c18b639a08344d40ac0c5a7306e546c3669
Obszczymucha <obszczymucha@gmail.com>
2024-08-20 19:00:15 +1000

    Ok, fuck tokyonight lol.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ return {
   -- the colorscheme should be available when starting Neovim
   {
     "folke/tokyonight.nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    lazy = true,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     opts = {
       -- your configuration comes here
@@ -45,7 +45,7 @@ return {
   },
   {
     "rebelot/kanagawa.nvim",
-    lazy = true,
+    lazy = false,
     config = function()
       require( 'kanagawa' ).setup( {
         compile = false,  -- enable compiling the colorscheme
