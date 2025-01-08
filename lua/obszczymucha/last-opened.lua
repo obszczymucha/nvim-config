f79084e802e340e5cc24fac0ff918442e194d535
Obszczymucha <obszczymucha@gmail.com>
2025-01-09 09:46:12 +1100

    Add some safeguards for last-opened.

--- lua/obszczymucha/last-opened.lua
+++ lua/obszczymucha/last-opened.lua
@@ -6,9 +6,13 @@ local group = vim.api.nvim_create_augroup( "LastOpened", { clear = true } )
 
 local function on_leave_pre()
   local bufnr = vim.api.nvim_get_current_buf()
+  local bufname = vim.api.nvim_buf_get_name( bufnr )
+
+  if not bufname or bufname:match("^oil:///") then return end
+
   local cursor = vim.api.nvim_win_get_cursor( 0 )
   local data = {
-    filename = vim.api.nvim_buf_get_name( bufnr ),
+    filename = bufname,
     line = cursor[ 1 ],
     col = cursor[ 2 ]
   }
@@ -28,6 +32,17 @@ local function on_enter()
 
     vim.defer_fn( function()
       vim.cmd( "edit " .. data.filename )
+
+      local buf = vim.api.nvim_win_get_buf( 0 )
+      local line_count = vim.api.nvim_buf_line_count(buf)
+      local target_line = data.line - 1
+
+      if target_line >= line_count then return end
+
+      local line = vim.api.nvim_buf_get_lines(buf, target_line, target_line + 1, true)[1]
+      local target_col = data.col - 1
+
+      if target_col > #line then return end
 
       if data.line and data.col then
         vim.api.nvim_win_set_cursor( 0, { data.line, data.col } )
