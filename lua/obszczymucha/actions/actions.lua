4faee4d1727bbdae27b42ed5bbdb0b93182a89fa
Obszczymucha <obszczymucha@gmail.com>
2025-07-26 17:50:52 +1000

    Add relative path copying/yanking to Actions.

--- lua/obszczymucha/actions/actions.lua
+++ lua/obszczymucha/actions/actions.lua
@@ -7,9 +7,23 @@ return {
     end
   },
   {
+    name = "Copy current file relative path",
+    action = function()
+      vim.fn.setreg( "+", vim.fn.expand( "%:." ) )
+      vim.notify( "Current file path copied" )
+    end
+  },
+  {
     name = "Yank current file path",
     action = function()
       vim.fn.setreg( '"', vim.fn.expand( "%:p" ) )
+      vim.notify( "Current file path yanked" )
+    end
+  },
+  {
+    name = "Yank current file relative path",
+    action = function()
+      vim.fn.setreg( '"', vim.fn.expand( "%:." ) )
       vim.notify( "Current file path yanked" )
     end
   }
