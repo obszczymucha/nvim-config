48e99c12e5d99312902c2e0ea8f0c1cac38f4442
Obszczymucha <obszczymucha@gmail.com>
2025-07-27 19:36:05 +1000

    Add formatting keybind using clangd-format for gdshader filetype.

--- ftplugin/gdshader.lua
+++ ftplugin/gdshader.lua
@@ -1,1 +1,21 @@
 vim.opt_local.commentstring = "// %s"
+
+local function has_clang_format()
+  local handle = io.popen( "command -v clang-format" )
+
+  if handle then
+    local result = handle:read( "*a" )
+    handle:close()
+    return result and result:len() > 0
+  end
+
+  return false
+end
+
+if has_clang_format() then
+  vim.keymap.set( "n", "<leader>F", ":%!clang-format<CR>", { desc = "Format", silent = true } )
+else
+  vim.defer_fn( function()
+    vim.notify( "clang-format is not installed, formatting unavailable", vim.log.levels.WARN )
+  end, 1500 )
+end
