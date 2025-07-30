fde166f4ae63ea61fd3c2f2b85ad43f3684e656d
Obszczymucha <obszczymucha@gmail.com>
2025-07-30 20:22:05 +1000

    Fix last-opened module.

--- lua/obszczymucha/last-opened.lua
+++ lua/obszczymucha/last-opened.lua
@@ -1,33 +1,42 @@
 local M = {}
 
 local config = require( "obszczymucha.user-config" )
-local utils = require( "obszczymucha.utils" )
+local state = require( "obszczymucha.state.last-opened" )
+
+local ignore_filetypes = { "codecompanion", "gdshader", "oil", "qf", "query" }
 
 local group = vim.api.nvim_create_augroup( "LastOpened", { clear = true } )
 
-local function starts_with( str, prefix )
-  if not prefix then return false end
-  return str:sub( 1, #prefix ) == prefix
-end
-
-local function on_leave_pre()
+local function on_buf_leave()
   local bufnr = vim.api.nvim_get_current_buf()
+  local filetype = vim.api.nvim_get_option_value( "filetype", { buf = bufnr } )
+
+  if vim.tbl_contains( ignore_filetypes, filetype ) then return end
+
   local bufname = vim.api.nvim_buf_get_name( bufnr )
-
-  if not bufname or bufname:match( "^oil:///" ) then return end
-
-  local root_dir = utils.get_project_root_dir()
-
-  if not starts_with( bufname, root_dir ) then return end
-
   local cursor = vim.api.nvim_win_get_cursor( 0 )
-  local data = {
-    filename = bufname,
+
+  if not filetype or filetype == "" then return end
+
+  state.last_buffer = {
+    name = bufname,
+    filetype = filetype,
+    path = vim.fn.expand( bufname ),
     line = cursor[ 1 ],
     col = cursor[ 2 ]
   }
+end
 
-  config.set_local( "last-opened", data )
+vim.api.nvim_create_autocmd( "BufLeave", {
+  group = group,
+  callback = on_buf_leave
+} )
+
+local function on_leave_pre()
+  on_buf_leave()
+  if not state.last_buffer then return end
+
+  config.set_local( "last-opened", state.last_buffer )
 end
 
 vim.api.nvim_create_autocmd( "VimLeavePre", {
@@ -36,29 +45,30 @@ vim.api.nvim_create_autocmd( "VimLeavePre", {
 } )
 
 local function on_enter()
-  if vim.fn.argc() == 0 then
-    local data = config.get_local( "last-opened" )
-    if not data or not data.filename or vim.fn.filereadable( data.filename ) == 0 then return end
+  if vim.fn.argc() > 0 then return end
 
-    vim.schedule( function()
-      vim.cmd( "edit " .. data.filename )
+  local data = config.get_local( "last-opened" )
+  if not data or not data.path or vim.fn.filereadable( data.path ) == 0 then return end
 
-      local buf = vim.api.nvim_win_get_buf( 0 )
-      local line_count = vim.api.nvim_buf_line_count( buf )
-      local target_line = data.line - 1
+  vim.schedule( function()
+    vim.cmd( "edit " .. data.path )
 
-      if target_line >= line_count then return end
+    local buf = vim.api.nvim_win_get_buf( 0 )
+    local line_count = vim.api.nvim_buf_line_count( buf )
+    local target_line = data.line - 1
 
-      local line = vim.api.nvim_buf_get_lines( buf, target_line, target_line + 1, true )[ 1 ]
-      local target_col = data.col - 1
+    if target_line >= line_count then return end
 
-      if target_col > #line then return end
+    local line = vim.api.nvim_buf_get_lines( buf, target_line, target_line + 1, true )[ 1 ]
+    local target_col = data.col - 1
 
-      if data.line and data.col then
-        vim.api.nvim_win_set_cursor( 0, { data.line, data.col } )
-      end
-    end )
-  end
+    if target_col > #line then return end
+
+    if data.line and data.col then
+      vim.api.nvim_win_set_cursor( 0, { data.line, data.col } )
+      vim.cmd( "normal! zz" )
+    end
+  end )
 end
 
 vim.api.nvim_create_autocmd( "VimEnter", {
---
+++ lua/obszczymucha/state/last-opened.lua
@@ -0,0 +1,5 @@
+-- I contain state for last-opened module.
+-- The module gets reloaded (on remap to refresh the code) while the state persists.
+local M = {}
+
+return M
