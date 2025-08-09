4d9c6008effd2a1e5ca259962e6fb5b29caae52e
Obszczymucha <obszczymucha@gmail.com>
2025-08-09 12:37:31 +1000

    Fix color-overrides.

--- lua/obszczymucha/color-overrides.lua
+++ lua/obszczymucha/color-overrides.lua
@@ -1,5 +1,5 @@
 local cmd = vim.cmd
-local utils = require("obszczymucha.utils")
+local utils = require( "obszczymucha.utils" )
 local saturate, brightness = utils.saturate, utils.brightness
 
 cmd( "autocmd ColorScheme * hi CursorLineNr ctermfg=214 ctermbg=237 guifg=#fabd2f guibg=None" )
@@ -13,20 +13,17 @@ cmd( "autocmd ColorScheme * highlight LspFloatWinNormal guibg=NONE" )
 local purple = "#9f7fff"
 local light_purple = saturate( purple, 0.6 )
 local light_purple2 = saturate( purple, 0.9 )
-local dark_purple = brightness(purple, 0.4)
+local dark_purple = brightness( purple, 0.4 )
 
 cmd( string.format( "hi WinSeparator guifg=%s guibg=NONE", dark_purple ) )
 
 vim.cmd( string.format( "highlight ActionsTelescopeBorder guifg=%s", light_purple ) )
 
-vim.api.nvim_set_hl( 0, "NotifyINFOIcon", { fg = "#b9c0eb" } )
-
-local function set_notify_highlights( prefix, color )
-  local info_highlights = { "Title", "Border", "Body" }
-
-  for _, suffix in ipairs( info_highlights ) do
+local function set_notify_highlights( prefix, suffixes, color )
+  for _, suffix in ipairs( suffixes ) do
     vim.api.nvim_set_hl( 0, prefix .. suffix, { fg = color } )
   end
 end
 
-set_notify_highlights( "NotifyINFO", light_purple2 )
+set_notify_highlights( "NotifyINFO", { "Title", "Border" }, light_purple2 )
+set_notify_highlights( "NotifyINFO", { "Icon", "Body" }, "#b9c0eb" )
