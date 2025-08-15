a011832ba584a7ace6d533d9b924f22448095c00
Obszczymucha <obszczymucha@gmail.com>
2025-08-15 15:27:11 +1000

    Prevent unnecessary searches in multigrep.

--- lua/obszczymucha/telescope/multigrep.lua
+++ lua/obszczymucha/telescope/multigrep.lua
@@ -9,6 +9,22 @@ local multigrep_core = require( "obszczymucha.telescope.multigrep_core" )
 
 M.generate_multigrep_command = multigrep_core.generate_multigrep_command
 
+local function prevent_duplicate_searches( picker, command_generator )
+  local original_on_lines = picker._on_lines
+  local last_command_str = nil
+
+  picker._on_lines = function( ... )
+    local prompt = picker:_get_prompt()
+    local command = command_generator( prompt )
+    local command_str = vim.inspect( command )
+
+    if last_command_str ~= command_str then
+      last_command_str = command_str
+      original_on_lines( ... )
+    end
+  end
+end
+
 function M.live_multigrep( search_term )
   local opts = {}
   opts.cwd = opts.cwd or vim.fn.getcwd()
@@ -25,9 +41,12 @@ function M.live_multigrep( search_term )
     finder = finder,
     previewer = conf.grep_previewer( opts ),
     sorter = require( "telescope.sorters" ).empty(),
-    attach_mappings = function( _, map )
-      map( "i", "<CR>", function( prompt_bufnr )
-        local result = actions.select_default( prompt_bufnr )
+    attach_mappings = function( prompt_bufnr, map )
+      local picker = require( "telescope.actions.state" ).get_current_picker( prompt_bufnr )
+      prevent_duplicate_searches( picker, M.generate_multigrep_command )
+
+      map( "i", "<CR>", function( bufnr )
+        local result = actions.select_default( bufnr )
         vim.schedule( function() vim.cmd( "normal! zz" ) end )
         return result
       end )
