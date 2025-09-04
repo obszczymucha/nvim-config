9176d7c0544dffc5f0df5fb3609cd06b7a6ad0e6
Obszczymucha <obszczymucha@gmail.com>
2025-09-05 09:58:08 +1000

    Adjust the background.

--- lua/obszczymucha/colorscheme/schemes.lua
+++ lua/obszczymucha/colorscheme/schemes.lua
@@ -172,7 +172,7 @@ M.schemes = {
       } ),
       overrides = {
         Visual = { bg = adjust( "#0074a3" ) },
-        Normal = { bg = "#223240" },
+        Normal = { bg = "#1e2c3a" },
         TelescopeNormal = { bg = adjust( "#000c10" ) },
         TelescopeBorder = { bg = adjust( "#000c10" ), fg = adjust( "#0074a3" ) },
         TelescopePromptNormal = { bg = adjust( "#000c10" ) },
