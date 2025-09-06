b9c940c4052cd40a52b08da6eaa38c902e103723
Obszczymucha <obszczymucha@gmail.com>
2025-09-06 20:56:50 +1000

    Revert s to default behaviour since flash is now enabled for search.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -8,14 +8,14 @@ return {
     modes = {
       search = {
         enabled = true,
-        highlight = { backdrop = false },
+        highlight = { backdrop = true },
         jump = { history = true, register = true, nohlsearch = true }
       }
     }
   },
   -- stylua: ignore
   keys = {
-    { "s",     mode = { "n", "x", "o" }, function() require( "flash" ).jump() end,              desc = "Flash" },
+    -- { "s",     mode = { "n", "x", "o" }, function() require( "flash" ).jump() end,              desc = "Flash" },
     { "<A-v>", mode = { "n", "x", "o" }, function() require( "flash" ).treesitter() end,        desc = "Flash Treesitter" },
     { "r",     mode = "o",               function() require( "flash" ).remote() end,            desc = "Remote Flash" },
     { "R",     mode = { "o", "x" },      function() require( "flash" ).treesitter_search() end, desc = "Treesitter Search" },
